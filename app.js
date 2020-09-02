56df9ccac1d00cdccabf18cb7c0e911249138d89
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-01 20:37:13 -0400

    basic landing page

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("landing page! - testing server connection")
+    res.render("landing")
 })
 
 app.listen(3000, function(){
