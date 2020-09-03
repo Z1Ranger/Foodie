68ac359c47775da3f7378df2df8e33278227e7b9
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-03 13:45:54 -0400

    Switch to MongoDB for data persistence

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
 var express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(()=>console.log('Connected to DB!'))
+    .catch(error => console.log(error.message))
+    ;
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Salmon Creek",
+//         image: "https://img.hipcamp.com/images/c_fill,f_auto,h_600,q_60,w_600/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj/salmon-creek-ranch-redwood-camp-bodega-bay-tent-forest-people.jpg"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             console.log("NEWLY CREATED CAMPGROUND: ");
+//             console.log(campground);
+//         }
+//     }
+// )
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://img.hipcamp.com/images/c_fill,f_auto,h_600,q_60,w_600/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj/salmon-creek-ranch-redwood-camp-bodega-bay-tent-forest-people.jpg"},
-    {name: "Granite Hill", image: "https://s3.amazonaws.com/centralcatalogue001/production/uploads/image/file/2439/MCU671_2_Tenting%20Sites.jpg"}
-];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    }); 
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampgrounds = {name: name, image: image};
-    campgrounds.push(newCampgrounds);
-    res.redirect("/campgrounds");
+    Campground.create(newCampgrounds, function(err, campground){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("NEWLY CREATED CAMPGROUND: ");
+            res.redirect("/campgrounds");
+        }
+    });
+    
 });
 
 app.get("/campgrounds/new", function(req, res){
