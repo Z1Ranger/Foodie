25ce72c2c56f94d07cde3181f8338c8d0f90ab7c
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-04 00:47:11 -0400

    add show route and page

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -21,7 +22,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Salmon Creek",
-//         image: "https://img.hipcamp.com/images/c_fill,f_auto,h_600,q_60,w_600/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj/salmon-creek-ranch-redwood-camp-bodega-bay-tent-forest-people.jpg"
+//         image: "https://img.hipcamp.com/images/c_fill,f_auto,h_600,q_60,w_600/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj/salmon-creek-ranch-redwood-camp-bodega-bay-tent-forest-people.jpg",
+//         description: "This is a huge granite hill, no bathrooms. No water."
 //     }, function(err, campground){
 //         if(err){
 //             console.log(err);
@@ -41,6 +43,8 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+
+//INDEX
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if(err){
@@ -52,10 +56,13 @@ app.get("/campgrounds", function(req, res){
     }); 
 });
 
+
+//CREATE
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampgrounds = {name: name, image: image};
+    var desc = req.body.desc;
+    var newCampgrounds = {name: name, image: image, description: desc};
     Campground.create(newCampgrounds, function(err, campground){
         if (err){
             console.log(err);
@@ -68,9 +75,23 @@ app.post("/campgrounds", function(req, res){
     
 });
 
+
+//NEW
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 })
+
+//SHOW
+app.get("/campgrounds/:id",function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
