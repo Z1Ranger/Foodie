0a81117fd01ba9add6b206da7a6c0e31409f6030
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-05 13:51:28 -0400

    Add seed file and comment sample comment feature

--- app.js
+++ app.js
@@ -2,39 +2,17 @@ var express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
 var mongoose = require("mongoose");
+var Campground = require("./models/campgrounds");
+var Comment = require("./models/comments");
+var seedDB = require("./seeds");
 
+seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     useNewUrlParser: true,
     useUnifiedTopology: true})
     .then(()=>console.log('Connected to DB!'))
     .catch(error => console.log(error.message))
     ;
-
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Salmon Creek",
-//         image: "https://img.hipcamp.com/images/c_fill,f_auto,h_600,q_60,w_600/v1445485223/campground-photos/fnqqusfbuyxsyrizknsj/salmon-creek-ranch-redwood-camp-bodega-bay-tent-forest-people.jpg",
-//         description: "This is a huge granite hill, no bathrooms. No water."
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             console.log("NEWLY CREATED CAMPGROUND: ");
-//             console.log(campground);
-//         }
-//     }
-// )
-
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -83,7 +61,7 @@ app.get("/campgrounds/new", function(req, res){
 
 //SHOW
 app.get("/campgrounds/:id",function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground){
         if(err){
             console.log(err);
         }
