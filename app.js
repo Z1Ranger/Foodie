a8675d342ef835784efc6a271c434480f6806746
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-18 18:04:56 -0400

    Add comment functionality and design modifications

--- app.js
+++ app.js
@@ -79,10 +79,38 @@ app.get("/campgrounds/:id",function(req, res){
 
 app.get("/campgrounds/:id/comments/new", function(req, res){
     var id = req.params.id;
-    res.render("comments/new")
+    Campground.findById(id, function(err, campground){
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    var id = req.params.id;
+    Campground.findById(id, function(err, campground){
+        if (err){
+            console.log(err);
+        }
+        else{
+            Comment.create(req.body.comment, function(err, comment){
+                if (err){
+                    console.log(err);
+                }
+                else{
+                    campground.comments.push(comment);;
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+
+            });
+        }
+    });
 });
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
 });
-
