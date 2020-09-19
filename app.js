b389d978ccbf8ad8d2f3aa166349f5fe09d2a34c
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-19 10:29:50 -0400

    Change to style

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
 seedDB();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
