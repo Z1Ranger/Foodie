8bbc0737de11eaf32183d52d57896b84619e84f1
Vishnu Varma <gvvarma2712@gmail.com>
2020-09-19 22:03:35 -0400

    Build basic login form

--- app.js
+++ app.js
@@ -148,6 +148,11 @@ app.post("/register", function(req, res){
     });
 });
 
+//show login form
+app.get("/register", function(req, res){
+    res.render("login");
+});
+
 
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
